5729ab0f6d28942fa7b5d12d56b4351e94e2dd1f
woshifrank <yy586@cornell.edu>
2023-07-21 15:01:28 -0400

    add component deletion

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {TASKS} from '../../mock-tasks'
 import {Task} from '../../Task'
+import {TaskService} from '../../services/task.service';
 
 @Component({
   selector: 'app-tasks',
@@ -8,12 +9,19 @@ import {Task} from '../../Task'
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit{
-  tasks: Task[] = TASKS
-  constructor() {
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) {
 
   }
 
   ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks) );
+  }
 
+  deleteTask(task : Task){
+    this.taskService.deleteTask(task).subscribe(
+      () => (this.tasks = this.tasks.filter(t => t.id !== task.id ))
+    )
   }
 }
